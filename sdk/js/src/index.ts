b9570e09686609cd9a6fdeed455e27a6621a0022
Oanakiaja <zhangjingtian.oana@bytedance.com>
2025-12-01 13:05:02 +0800

    fix(js): fix js sdk import error (#88)
    * fix(js): remove unused error exports
    
    * fix: add sdk/js into workspace to lock deps & fix export error
    
    * fix: cjs -> js
    
    * test: add cjs mjs import test
    
    * chore: v
    
    ---------
    
    Co-authored-by: jinxin001 <jinxin001@bytedance.com>

--- sdk/js/src/index.ts
+++ sdk/js/src/index.ts
@@ -1,6 +1,5 @@
 export * as SandboxApi from "./api/index.js";
 export type { BaseClientOptions, BaseRequestOptions } from "./BaseClient.js";
 export { SandboxClient } from "./Client.js";
-export { SandboxError, SandboxTimeoutError } from "./errors/index.js";
 export * from "./exports.js";
 export * as providers from "./providers/index.js";
